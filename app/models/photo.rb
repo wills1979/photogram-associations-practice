251373ba94ebb9c9f98e5e0936e1ee0ab5291cbb
wills1979 <107662428+wills1979@users.noreply.github.com>
2025-05-09 21:31:56 +0000

    add some associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -20,8 +20,10 @@ class Photo < ApplicationRecord
   ## Direct associations
 
   # Photo#poster: returns a row from the users table associated to this photo by the owner_id column
+  belongs_to(:poster, class_name: "User", foreign_key: "owner_id", primary_key: "id")
 
   # Photo#comments: returns rows from the comments table associated to this photo by the photo_id column
+  has_many(:comments, class_name: "Comment", foreign_key: "photo_id")
 
   # Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/", controller: "users", action: "index")
+  
   get("/users", controller: "users", action: "index")
   get("/users/:username", controller: "users", action: "show")
   post("/add_user", controller: "users", action: "create")
